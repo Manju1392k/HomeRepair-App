94256990cff3c857b10e2bab8bbaa03c8695f121
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-26 15:53:09 +0530

    HomeRepair

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.navbar}>
-        <Feather name="menu" size={30} color="white" />
-        <Image
-          source={require("./assets/profile.jpg")}
-          style={styles.profile}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.navbar}>
+          <Feather name="menu" size={30} color="white" />
+          <Image
+            source={require("./assets/profile.jpg")}
+            style={styles.profile}
+          />
+        </View>
+
+        <View style={styles.Searchnavbar}>
+          <TextInput placeholder="Search" style={styles.search} />
+        </View>
+
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -35,5 +49,18 @@ const styles = StyleSheet.create({
     height: 60,
     width: 60,
     borderRadius: 50,
+  },
+  Searchnavbar: {
+    width: 400,
+    height: 70,
+    backgroundColor: "#000",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  search: {
+    width: 350,
+    backgroundColor: "#fff",
+    padding: 5,
   },
 });
