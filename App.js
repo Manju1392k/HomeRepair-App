fb13ca9e1ad3bbfe088a3a8f66ae57921c226f7e
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-26 15:47:40 +0530

    HomeRepair

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.navbar}>
+        <Feather name="menu" size={30} color="white" />
+        <Image
+          source={require("./assets/profile.jpg")}
+          style={styles.profile}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+  },
+  navbar: {
+    marginTop: 24,
+    width: 400,
+    height: 70,
+    backgroundColor: "#000",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  profile: {
+    height: 60,
+    width: 60,
+    borderRadius: 50,
   },
 });
